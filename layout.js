d8d1aa029d57f6d214666afd7de1c5b6c2cc81fc
saptraining1788-ui <saptraining1788@gmail.com>
2026-05-09 20:49:07 +0530

    Delete layout.js

--- layout.js
+++
@@ -1,6 +0,0 @@
-
-export const metadata = { title: 'CoreKonnect', description: 'AI Services' };
-import './globals.css';
-export default function RootLayout({ children }) {
- return <html><body>{children}</body></html>;
-}
